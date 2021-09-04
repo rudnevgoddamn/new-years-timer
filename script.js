f39d38869d8c4a79b11332a5f6ff348dceba0e40
Егор Руднев <rudnievi1@gmail.com>
2021-09-04 21:30:52 +0300

    comment

---
+++ script.js
@@ -0,0 +1,31 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+
+
+
+
+const newYears = '1 Jan 2022';
+function countdown() {
+    const currentDate = new Date();
+    const newYearsDate = new Date(newYears);
+
+
+    const totalSeconds = (newYearsDate - currentDate) /
+     1000;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const hours = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds % 60);
+
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secondsEl.innerHTML = seconds;
+}
+//
+setInterval(countdown, 1000)
+countdown();
